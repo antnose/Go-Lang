5bdf0de94397a861858ccc87c56136ab8c4ece4b
antnose <antnose28@gmail.com>
2025-10-03 22:09:50 +0600

    Learn about if else if and else

--- Go-With-Habib/main.go
+++ Go-With-Habib/main.go
@@ -1,13 +1,29 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println("Hello World")
+// 	a := 10
+// 	a = 45
+// 	fmt.Println(a)
+// 	const b = "Rohim"
+// 	// b = "Karim"
+// 	fmt.Println(b)
+// }
+
+// If-else and switch
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
-	a := 10
-	a = 45
-	fmt.Println(a)
-	const b = "Rohim"
-	// b = "Karim"
-	fmt.Println(b)
+	age := 15
+	if age > 18 {
+		fmt.Println("You are eligible for married ")
+	} else if age < 18 {
+		fmt.Println("You are not eligible to be married, but you can love someone")
+	} else {
+		fmt.Println("You are just a teenager, not eligible for married")
+	}
 }
