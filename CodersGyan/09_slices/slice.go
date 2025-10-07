eb401795e4c72e964be05573e3197f0ced971aad
antnose <antnose28@gmail.com>
2025-10-07 17:04:14 +0600

    learn about dynamic array and some methods

--- CodersGyan/09_slices/slice.go
+++ CodersGyan/09_slices/slice.go
@@ -11,18 +11,55 @@ func main() {
 	// fmt.Println(nums == nil)
 	// fmt.Println(len(nums))
 
-	var nums = make([]int, 0, 5)
-	fmt.Println(cap(nums))
+	// var nums = make([]int, 0, 5)
+	// fmt.Println(cap(nums))
 	// fmt.Println(nums)
 	// fmt.Println(nums == nil)
 
-	nums = append(nums, 1)
-	nums = append(nums, 2)
-	nums = append(nums, 3)
-	nums = append(nums, 4)
-	nums = append(nums, 5)
-	nums = append(nums, 6)
-	nums = append(nums, 7)
+	// nums = append(nums, 1)
+	// nums = append(nums, 2)
+	// nums = append(nums, 3)
+	// nums = append(nums, 4)
+	// nums = append(nums, 5)
+	// nums = append(nums, 6)
+	// nums = append(nums, 7)
+	// fmt.Println(nums)
+	// fmt.Println(cap(nums))
+
+	// nums := []int{}
+	// fmt.Println(nums)
+	// fmt.Println(len(nums))
+	// nums = append(nums, 1)
+	// nums = append(nums, 2)
+	// fmt.Println(nums)
+	// fmt.Println(len(nums))
+
+	// var nums = make([]int, 1, 5)
+	// nums[0] = 1
+	// fmt.Println(nums)
+
+	// var nums = make([]int, 0, 5)
+	// nums = append(nums, 1)
+	// var nums2 = make([]int, len(nums))
+	// fmt.Println(len(nums2))
+
+	// fmt.Println(nums, nums2)
+
+	// copy(nums2, nums)
+	// fmt.Println(nums, nums2)
+
+	// slice operator
+	// var nums = []int{1, 2, 3, 4}
+	// fmt.Println(nums[0:1])
+	// fmt.Println(nums[0:])
+	// fmt.Println(nums[:2])
+
+	// slice
+	// var nums1 = []int{1, 2}
+	// var nums2 = []int{1, 2}
+	// fmt.Println(slices.Equal(nums1, nums2))
+
+	var nums = [][]int{{1, 2, 3, 4}, {4, 5, 6}}
 	fmt.Println(nums)
-	fmt.Println(cap(nums))
+
 }
