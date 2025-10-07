06420bc4243223f97971ed6f41b387fce207b6c7
antnose <antnose28@gmail.com>
2025-10-07 16:27:13 +0600

    Learn about array

--- CodersGyan/08_arrays/array.go
+++ CodersGyan/08_arrays/array.go
@@ -4,11 +4,36 @@ import "fmt"
 
 // numbered sequence of specific length
 func main() {
-	var nums [5]int
-	fmt.Println(len(nums))
+	// var nums [5]int
+	// fmt.Println(len(nums))
 
-	num := 20
-	fmt.Println(num)
+	// add value in array
+	// nums[0] = 1
+	// nums[1] = 2
+	// nums[2] = 3
+	// nums[3] = 4
+	// nums[4] = 5
+	// fmt.Println(nums)
+
+	// num := 20
+	// fmt.Println(num)
+
+	// Boolean array
+	// var vals [4]bool
+	// vals[2] = true
+	// fmt.Println(vals)
+
+	// String array
+	// var name [5]string
+	// name[0] = "Rohim"
+	// name[4] = "Badsha"
+	// fmt.Println(name)
+
+	// to declare it in single line
+	// dec := [3]int{1, 2, 3}
+	// fmt.Println(dec)
+
+	// 2d array
+	arrTwoD := [2][2]int{{1, 2}, {3, 4}}
+	fmt.Println(arrTwoD)
 }
-
-// 1.04.27
